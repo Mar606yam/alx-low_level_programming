7685d79870dbf4d9dd0b7e550573a031a8b23b10
Mar606yam <maryamtalaat844@gmail.com>
2023-10-18 12:11:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description main : main - A C program that prints with printf function.
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
