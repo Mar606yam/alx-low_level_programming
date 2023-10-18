ef9def0eddafd2be13b0e0fd68f543dd6aa53fb6
Mar606yam <maryamtalaat844@gmail.com>
2023-10-18 13:52:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Description main : main - A program that prints a text using write function.
+ *             write(int fd , constant void *buf , size_t count)
+ * Return:1
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	Write(1, quo, 59);
+	return (1);
+}
