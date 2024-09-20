d373c2279df9d723806fd7722675cb4c8981caa5
maryamtalaat <maryamtalaat844@gmail.com>
2024-09-19 21:45:28 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: a program that prints the alphabet in lowercase.
+*
+* Return: 0 on success, 1 on error
+*/
+int main(void)
+{
+	char x;
+	int i;
+
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
